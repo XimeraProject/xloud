95d6e794527c7c5ecbfe17b326a9dc1fe17083b8
Jim Fowler <fowler@math.osu.edu>
2019-10-03 16:18:19 -0400

    do cloning inside webworker

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import 'bootstrap';
 import './scss/app.scss';
-import * as git from 'isomorphic-git';
+
 import * as pify from 'pify';
 import { version } from '../package.json';
 
 const repositoryName = 'mooculus/calculus';
 
-import * as LightningFS from '@isomorphic-git/lightning-fs';
+import * as git from 'isomorphic-git';
+import FS from '@isomorphic-git/lightning-fs';
 
-const fs = new LightningFS(repositoryName);
+const fs = new FS(repositoryName);
 git.plugins.set('fs', fs);
+
 const pfs = fs.promises;
-window.pfs = fs.promises;
 
 console.log("This is");
 console.log("  ▀██▄   ▄██▀ ██ █████     █████ ▄███████████████████▄    ███");
@@ -23,28 +24,30 @@ console.log("version",version);
 
 let directoryName = '/repository';
 
-let worker = new Worker('worker.js');
-worker.postMessage('from me!');
-worker.postMessage('againfrom me!');
+function cloneFromGithub( repositoryName : string, directoryName : string, progress : any = undefined ) {
+    return new Promise(function(resolve, reject) {
+	let worker = new Worker('worker.js');
 
+	worker.addEventListener('message', function(e) {
+	    if (e.data.message == 'progress') {
+		if (progress)
+		    progress( e.data.progress );
+	    }
+	    
+	    if (e.data.message == 'cloned') {
+		resolve();
+	    }
 
-// TODO: factor this out into a webworker
-// see https://stackoverflow.com/questions/34851839/how-to-handle-web-workers-standard-syntax-with-webpack
-// and also https://isomorphic-git.org/docs/en/webworker
-async function cloneRepository() {
-    console.log("Cloning...");
-    
-    await git.clone({
-	dir: directoryName,
-	corsProxy: 'https://cors.isomorphic-git.org',
-	url: 'https://github.com/' + repositoryName,
-	ref: 'master',
-	singleBranch: true,
-	depth: 2
+	    if (e.data.message == 'error') {
+		reject(e.data.error);
+	    }
+	});
+	
+	worker.postMessage( { repositoryName: repositoryName,
+			      directoryName: directoryName } );
     });
-    
-    console.log("Cloned!");
 }
+
 
 async function main() {
     try {
@@ -52,11 +55,16 @@ async function main() {
 	    console.log( "Already exists" );
 	} else {
 	    console.log("Could not access it.");
-	    await cloneRepository();
+	    await cloneFromGithub( repositoryName, directoryName );
 	}
     } catch (err) {
 	console.log("Directory likely does not exist.");
-	await cloneRepository();
+
+	try {
+	    await cloneFromGithub( repositoryName, directoryName );
+	} catch (err) {
+	    console.log("Could not clone: ",err);
+	}
     }
 
     // Now it should not be empty...
--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,51 @@
-console.log("WEB WORKER: launched!");
+import * as git from 'isomorphic-git';
+import FS from '@isomorphic-git/lightning-fs';
 
-onmessage = function(e) {
-    console.log('WEB WORKER: received',e);
-    //postMessage(workerResult);
-}
+////////////////////////////////////////////////////////////////
+import EventEmitter from 'events';
+const emitter = new EventEmitter();
+
+emitter.on('message', function(message) {
+    console.log("git:",message);
+});
+
+emitter.on('progress', function(progress) {
+    console.log("git:",progress);
+    let percent = Math.floor(100 * progress.loaded / progress.total) + '%';
+    self.postMessage({ message: 'progress', progress: progress });
+});
+
+git.plugins.set('emitter', emitter);
+
+////////////////////////////////////////////////////////////////
+console.log("git: worker launched");
+
+self.onmessage = async function (e : any) {
+    let repositoryName = e.data.repositoryName;
+    let directoryName = e.data.directoryName;
+
+    const fs = new FS(repositoryName);
+    git.plugins.set('fs', fs);
+
+    console.log("git: cloning " + repositoryName + "...");
+
+    try {
+	await git.clone({
+	    dir: directoryName,
+	    corsProxy: 'https://cors.isomorphic-git.org',
+	    url: 'https://github.com/' + repositoryName,
+	    ref: 'master',
+	    singleBranch: true,
+	    depth: 2
+	});
+    } catch (err) {
+	console.log("git: failed to clone " + repositoryName);
+	console.log("git: ",err);
+	self.postMessage({ message: 'error', error: err.toString() });
+	self.close();
+    }
+    
+    console.log("git: cloned " + repositoryName + "!");
+    self.postMessage({ message: 'cloned' });
+    self.close();
+};
